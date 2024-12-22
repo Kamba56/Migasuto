f7b91789b2d06e8dfaaea0d0aaf786c024ac618d
Toma John Musa <tomajohn316@yahoo.com>
2024-12-22 15:27:43 +0100

    Add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
+import { createBrowserRouter } from "react-router"
+import Authentication from "./pages/Authentication"
+import SignUp from "./pages/SignUp"
+import { RouterProvider } from "react-router-dom"
 
+const router = createBrowserRouter([
+  {
+    path: '/auth',
+    element: <Authentication/>,
+    children: [
+      {
+        path: 'signup',
+        element: <SignUp />,
+      }
+    ]
+  }
+])
 
 const App = () => {
 
-  return (
-    <>
 
-    </>
+  return (
+    <RouterProvider router={router} />
   )
 }
 
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,19 @@
+const SignUp = () => {
+  return (
+    <section>
+      <section>
+        <div>
+          <h1>Welcome to migasuto</h1>
+          <p>We are dedicated to providing high-quality research and analysis to help our clients make informed decisions. We put our clients first in every decision we make, which is why we will always succeed in delivering first- class solutions to all our client&apos;s challenges.</p>
+        </div>
+
+        <div>
+          <h2>Brand Equity Report</h2>
+          <p>Effortlessly work together with your team in real-time.</p>
+        </div>
+      </section>
+    </section>
+  )
+}
+
+export default SignUp;
